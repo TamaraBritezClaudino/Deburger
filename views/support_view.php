d5f3b97d3814274420c8f36d733fa8fb1e1e7043
TamaraBritezClaudino <tamarabritezc@gmail.com>
2026-09-22 00:31:41 -0300

    subo cambios

--- views/support_view.php
+++ views/support_view.php
@@ -5,5 +5,47 @@
 </head>
 
 <main>
-    
+    <article class="form">
+        <div>
+            <h2>Realizar un reclamo</h2>
+            <p>Si tu pedido fue online y tenes algun inconveniente, completa el siguiente formulario</p>
+        </div>
+
+        <form method="post">
+            <div>
+                <label for="nombre">Nombre y apellido</label>
+                <input type="text" id="nombre" name="nombre">
+            </div>
+
+            <div>
+                <div>
+                    <label for="correo">Correo electrónico</label>
+                    <input type="email" name="correo" id="correo">
+                </div>
+
+                <div>
+                    <label for="telefono">Teléfono (opcional)</label>
+                    <input type="tel" id="telefono" name="telefono">
+                </div>
+            </div>
+
+            <div>
+                <label for="reclamo">Detalle de tu reclamo</label>
+                <textarea name="reclamo" id="reclamo"></textarea>
+            </div>
+
+            <button type="submit" name="submit">
+                <p>Enviar</p>
+                <i class="bi bi-arrow-right"></i>
+            </button>
+
+        </form>
+
+        <p>
+            ¿Ya tienes una cuenta?
+            <a href="login.php">Inicia sesión</a>
+        </p>
+    </article>
+
+<!--A partir de aca podriamos poner preguntas frecuentes y medio de contacto... tengo sueño, me quiero jubilar y ni empece a trabajar AHHHHHHHHHHHHHHHHHHHHHHHHHHHH-->
 </main>
