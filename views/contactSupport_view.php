c4b0761212d69ff59b8e78b7700ddf3576edbf00
TamaraBritezClaudino <tamarabritezc@gmail.com>
2026-09-24 17:48:06 -0300

    subo cambios

--- views/contactSupport_view.php
+++ views/contactSupport_view.php
@@ -4,10 +4,22 @@
     <title>Soporte</title>
 </head>
 
-<main>
+<main id="contactSuportMain">
     <section>
-
+        <i class="bi bi-envelope"></i>
+        <h1>Soporte</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit morbi, dui fringilla metus maecenas erat sem malesuada, vestibulum id sociosqu vulputate eros scelerisque eget. Eget mattis odio fames ullamcorper mollis tristique himenaeos metus, inceptos convallis phasellus malesuada turpis pellentesque mauris litora aliquam, volutpat per platea arcu ligula sed vestibulum.</p>
+        <hr>
+        <div>
+            <i class="bi bi-envelope"></i>
+            <p>soporte@deburger.com</p>
+        </div>
+        <div>
+            <i class="bi bi-telephone"></i>
+            <p>xx-xxxxxxx</p>
+        </div>
     </section>
+
     <section class="form">
         <div>
             <h2>Realizar un reclamo</h2>
