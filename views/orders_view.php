827aa04404eb85818a3148aa75fd177018c08e35
TamaraBritezClaudino <tamarabritezc@gmail.com>
2026-09-25 00:52:02 -0300

    subo cambios

--- views/orders_view.php
+++ views/orders_view.php
@@ -8,7 +8,47 @@
     <h1>Historial de pedidos</h1>
 
     <div class="historial">
-        <i class="bi bi-box-seam"></i>
-        <p>Todavia no hiciste ningun pedido</p>
+        <div class="historialVacio"> 
+            <i class="bi bi-box-seam"></i>
+            <p>Todavia no hiciste ningun pedido</p>
+        </div>
+
+        <div class="pedido">
+            <div class="pedidoCabecera">
+                <div>
+                    <h2>Pedido #001</h2>
+                    <p>25/09/2026</p>
+                </div>
+
+                <span class="estadoPedido">Confirmado</span>
+            </div>
+
+            <hr>
+
+            <div class="productosPedido">
+                <div>
+                    <p>Hamburguesa clásica x2</p>
+                    <p>$18.000</p>
+                </div>
+
+                <div>
+                    <p>Papas fritas x1</p>
+                    <p>$4.500</p>
+                </div>
+
+                <div>
+                    <p>Coca-Cola x2</p>
+                    <p>$6.000</p>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="totalPedido">
+                <p>Total:</p>
+                <strong>$28.500</strong>
+            </div>
+        </div>
+
     </div>
 </main>
